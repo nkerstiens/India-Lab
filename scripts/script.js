f4e205a2f4fa1986f71e81de8fa45c12a42920a7
Noah Kerstiens <44041365+nkerstiens@users.noreply.github.com>
2021-11-02 15:04:26 -0500

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+$(function () {
+  $("#inputUserName").blur(function () {
+    let userName = $(this).val();
+    // window.alert(userName);
+  });
+});
+function alphaOnly(event) {
+  alert(event);
+
+  var key;
+
+  if (window.event) {
+    //   This is the Java off of India-Build!!
+  }
+}
